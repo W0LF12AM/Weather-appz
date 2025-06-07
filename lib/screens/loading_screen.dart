caf00bb29e941af3324879b780b34179c795f1f6
W0LF12AM <bayui4742@gmail.com>
2025-06-07 21:41:06 +0700

    initial

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+final LocationSettings locationSettings = LocationSettings(
+  accuracy: LocationAccuracy.low,
+  distanceFilter: 100,
+);
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Cek apakah layanan lokasi nyala
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      print('Layanan lokasi mati. Harap aktifkan di pengaturan.');
+      return;
+    }
+
+    // Cek permission
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        print('Permission lokasi ditolak.');
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      print('Permission ditolak permanen. Harap ubah di pengaturan.');
+      return;
+    }
+
+    // Kalau sudah dapat izin
+    print('Permission granted!');
+  }
+
+  void getLocation() async {
+
+    await handleLocationPermission();
+    Position position =
+        await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              getLocation();
+            },
+            child: Text('Get Location')),
+      ),
+    );
+  }
+}
