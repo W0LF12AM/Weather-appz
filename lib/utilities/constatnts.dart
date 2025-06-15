23e6c2385b09ac9680d6baeef6d0cfd6125b7b72
W0LF12AM <bayui4742@gmail.com>
2025-06-16 00:13:58 +0700

    completed

--- lib/utilities/constatnts.dart
+++ lib/utilities/constatnts.dart
@@ -18,21 +18,21 @@ const TexfieldInputDecoration = InputDecoration(
         borderRadius: BorderRadius.all(Radius.circular(10)),
         borderSide: BorderSide(color: Colors.black, width: 2)));
 
-final List<Color> morningGradient = [
-  Color(0xFFADD8E6), // Light Blue
-  Color(0xFF87CEEB), // Sky Blue
-];
+// final List<Color> morningGradient = [
+//   Color(0xFFADD8E6), // Light Blue
+//   Color(0xFF87CEEB), // Sky Blue
+// ];
 
-final List<Color> afternoonGradient = [
-  Color(0xFF87CEEB), // Sky Blue
-  Color(0xFFFFD700), // Golden Yellow
-];
+// final List<Color> afternoonGradient = [
+//   Color(0xFF87CEEB), // Sky Blue
+//   Color(0xFFFFD700), // Golden Yellow
+// ];
 
-final List<Color> eveningGradient = [
-  Color(0xFFFFA500), // Orange
-  Color(0xFF800080), // Purple
-  Color(0xFF191970), // Midnight Blue
-];
+// final List<Color> eveningGradient = [
+//   Color(0xFFFFA500), // Orange
+//   Color(0xFF800080), // Purple
+//   Color(0xFF191970), // Midnight Blue
+// ];
 
 final List<Color> nightGradient = [
   Color(0xFF191970), // Midnight Blue
